13f14907efe09cee0e0084edacf865395f0bbbcd
Robert Brown <ammoknight@gmail.com>
2025-08-07 16:42:03 -0600

    Remove unneeded comments

--- kino_redis_cluster/lib/kino_redis_cluster/application.ex
+++ kino_redis_cluster/lib/kino_redis_cluster/application.ex
@@ -11,13 +11,8 @@ defmodule KinoRedisCluster.Application do
     Kino.SmartCell.register(Livebook.SmartCell.RedisCluster.Connect)
     Kino.SmartCell.register(Livebook.SmartCell.RedisCluster.Pipeline)
 
-    children = [
-      # Starts a worker by calling: KinoRedisCluster.Worker.start_link(arg)
-      # {KinoRedisCluster.Worker, arg}
-    ]
+    children = []
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KinoRedisCluster.Supervisor]
     Supervisor.start_link(children, opts)
   end
