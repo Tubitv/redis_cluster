97dd4762b7ca14494dad141ef91f0ab2022261ca
Robert Brown <rbrown@tubi.tv>
2026-03-03 10:26:50 -0700

    Publish kino_redis_cluster and update Livebook

--- kino_redis_cluster/mix.exs
+++ kino_redis_cluster/mix.exs
@@ -1,12 +1,18 @@
 defmodule KinoRedisCluster.MixProject do
   use Mix.Project
 
+  @homepage_url "https://hexdocs.pm/kino_redis_cluster"
+  @source_url "https://github.com/Tubitv/redis_cluster"
+  @version "0.1.0"
+
   def project do
     [
       app: :kino_redis_cluster,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -20,9 +26,30 @@ defmodule KinoRedisCluster.MixProject do
 
   defp deps do
     [
-      {:redis_cluster, path: ".."},
+      {:redis_cluster, "~> 0.8"},
       {:kino, "~> 0.16"},
-      {:jason, "~> 1.4"}
+      {:jason, "~> 1.4"},
+      {:ex_doc, "~> 0.34", only: [:dev], runtime: false}
+    ]
+  end
+
+  def package do
+    %{
+      description: "Livebook extension for RedisCluster",
+      files: ~w(.formatter.exs mix.exs lib),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    }
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      homepage_url: @homepage_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
